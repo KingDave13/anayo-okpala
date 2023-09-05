1c765e8a5450462adf31b5f9f4a671938c79b4dd
David Okaliwe <okaliwedavid@gmail.com>
2023-09-05 01:51:51 +0100

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
+        'product-sans': ["Product Sans", "sans-serif"],
       },
     },
     screens: {

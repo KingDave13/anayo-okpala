e1f31bdf722aa9b2531f459531faad78e40e8fa1
David Okaliwe <okaliwedavid@gmail.com>
2023-09-06 00:18:43 +0100

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,7 +47,38 @@ const Navbar = () => {
                 
                 {/* FOR MOBILE */}
 
-                
+                <div className='sm:hidden flex flex-1 justify-end items-center'>
+                <img 
+                src={toggle ? close : menu}
+                alt='menu'
+                className='w-[28px] h-[28px] object-contain cursor-pointer'
+                onClick={() => setToggle(!toggle)}
+                />
+
+                <div className={`${!toggle ? 'hidden' 
+                : 'flex'} p-6 black-gradient absolute top-10 right-0 
+                mx-4 my-8 min-w-[140px] z-10 rounded-xl`}>
+                    <ul className='list-none flex justify-end items-start 
+                    flex-col gap-4'>
+                    {navLinks.map((link) => (
+                        <li
+                        key={link.id}
+                        className={`${
+                            active === link.title
+                            ? 'text-white'
+                            : 'text-secondary'
+                        } font-poppins font-medium cursor-pointer text-[16px]`}
+                        onClick={() => {
+                            setToggle(!toggle);
+                            setActive(link.title);
+                        }}
+                        >
+                        <a href={`#${link.id}`}>{link.title}</a>
+                        </li>
+                    ))}
+                    </ul>
+                </div>
+                </div>
             </div>
         </nav>
     )

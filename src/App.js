cd99e4ed3032228f50d7c2bcb9ca76efe1401c01
David Okaliwe <okaliwedavid@gmail.com>
2023-09-05 22:36:47 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
 import { Navbar } from './components';
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar />
+      </div>
+    </BrowserRouter>
   )
 };
 

976cc85ea23bd2e6f06c405109404d0af7ed2878
David Okaliwe <okaliwedavid@gmail.com>
2023-09-06 02:45:57 +0100

    Update

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,11 @@
 import logo from './logo.png';
 import close from './close.svg';
 import menu from './menu.svg';
+import hero from './hero.png'
 
 export {
     logo,
     menu,
     close,
+    hero,
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,32 @@
-import styles from '../styles';
+import styles, { layout } from '../styles';
 import { motion } from 'framer-motion';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+import { hero } from '../assets';
 
 const Hero = () => {
   return (
-    <section className='relative bg-primary w-full h-screen mx-auto'>
-        <div>
+    <section className='relative bg-primary w-full h-screen mx-auto flex
+    items-center'>
+        <div className='absolute items-center
+        max-w-7xl mx-auto flex flex-row items-start gap-5'
+        >
+            <motion.div variants={textVariant()}
+            className={`${layout.sectionInfo}`}
+            >
+                <h1 className='text-secondary font-bold text-[72px]'>
+                    No. 1 <span className='text-white'>
+                    Home accessories and general &nbsp;</span> 
+                    industrial materials supplier
+                </h1>
+            </motion.div>
 
-        </div>
-
-        <div>
-
+            <motion.div variants={fadeIn()}>
+                <img src={hero}/>
+            </motion.div>
         </div>
     </section>  
   )
 };
 
-export default Hero;
+export default SectionWrapper(Hero, '');

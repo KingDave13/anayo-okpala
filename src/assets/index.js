8dcc2f5505ce30f3d952b3e18e8b325f305883fe
David Okaliwe <okaliwedavid@gmail.com>
2023-09-09 07:58:37 +0100

    Update

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,6 +7,9 @@ import cloth from './cloth.png';
 import product1 from './product1.png';
 import product2 from './product2.png';
 import product3 from './product3.png';
+import vector from './Vector.png';
+import vector2 from './Vector2.png';
+import vector3 from './Vector3.png';
 
 export {
     logo,
@@ -18,4 +21,7 @@ export {
     product1,
     product2,
     product3,
+    vector,
+    vector2,
+    vector3,
 }

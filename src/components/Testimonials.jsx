fe5e6c132b8fff560a974db65d510d1bacf6cc42
David Okaliwe <okaliwedavid@gmail.com>
2023-09-09 13:02:02 +0100

    Update

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -8,31 +8,37 @@ const TestCard = ({ index, testimonial, image, name, designation }) => {
     return (
         <motion.div
         variants={fadeIn('', 'spring', index * 0.5, 0.75)}
-        className='bg-dimWhite pt-10 p-5 rounded-3xl xs:w-[500px] w-full'
+        className='bg-dimWhite pt-16 pb-16 p-10 rounded-3xl xs:w-[500px] 
+        w-full relative'
         >
-            <p className='text-secondary font-bold text-[48px]'>"</p>
+            <p className='text-secondary font-bold text-[48px] font-poppins
+            absolute'>
+                "
+            </p>
 
             <div className=''>
                 <p className='text-primary text-[18px] font-medium'>
                     {testimonial}
                 </p>
 
-                <div className='mt-7 flex justify-between items-center
-                 gap-1'>
-                <div className='flex-1 flex flex-col'>
-                    <p className='text-primary font-bold text-[18px]'>
-                        {name}
+                <div className='mt-2 flex justify-between items-center'>
+                    <img 
+                        src={image}
+                        alt='name'
+                        className='w-10 h-10 mr-5 rounded-full 
+                        object-cover'
+                    />
+                    <p className='text-[45px] mr-5 font-poppins-100'>
+                        |
                     </p>
-                    <p className='text-primary text-[12px]'>
-                        {designation}
-                    </p>
-                </div>
-
-                <img 
-                    src={image}
-                    alt={`feedback-by-${name}`}
-                    className='w-10 h-10 rounded-full object-cover'
-                />
+                    <div className='flex-1 flex flex-col'>
+                        <p className='text-primary font-bold text-[18px]'>
+                            {name}
+                        </p>
+                        <p className='text-primary text-[12px]'>
+                            {designation}
+                        </p>
+                    </div>
                 </div>
             </div>
         </motion.div>
@@ -42,7 +48,7 @@ const TestCard = ({ index, testimonial, image, name, designation }) => {
 const Testimonials = () => {
   return (
     <section className='relative w-full min-h-[900px] mx-auto flex'>
-        <div className='max-w-[95rem] mx-auto flex flex-col gap-5 mb-10'
+        <div className='max-w-[95rem] mx-auto flex flex-col mb-10'
         >
             <motion.div variants={textVariant()}
             className={`${layout.sectionInfo}`}
@@ -58,8 +64,7 @@ const Testimonials = () => {
                 </div>                  
             </motion.div>
 
-            <div className='flex gap-16
-            flex-wrap justify-center'>
+            <div className='flex gap-14 flex-wrap justify-center'>
                 {testimonials.map((test, index) => (
                 <TestCard 
                     key={test.name} 

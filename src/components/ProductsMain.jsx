ceec8fe3f11ec05700b93dd78a7c3144557034b9
David Okaliwe <okaliwedavid@gmail.com>
2023-09-25 13:27:31 +0100

    Update

--- src/components/ProductsMain.jsx
+++ src/components/ProductsMain.jsx
@@ -26,7 +26,7 @@ const AboutMain = () => {
             className='flex-1 md:ml-40 ss:ml-20'
             >
                 <h1 className='text-primary font-bold md:text-[45px]
-                ss:text-[40px] text-[40px] tracking-tight'>
+                ss:text-[35px] text-[40px] tracking-tight'>
                     Clothing Materials
                 </h1>
                 
@@ -84,7 +84,7 @@ const AboutMain = () => {
             className='flex-1 md:ml-40 ss:ml-20'
             >
                 <h1 className='text-primary font-bold md:text-[45px]
-                ss:text-[40px] text-[40px] tracking-tight'>
+                ss:text-[35px] text-[40px] tracking-tight'>
                     Tape
                 </h1>
                 
@@ -115,7 +115,7 @@ const AboutMain = () => {
                     </button>
 
                     <button className='border-[1px] border-primary 
-                    md:text-[14px] ss:text-[17px] text-[12px] 
+                    md:text-[14px] ss:text-[14px] text-[12px] 
                     md:py-2 ss:py-2 py-1 md:px-12 ss:px-8 
                     px-3 text-primary rounded-[3px] font-medium 
                     grow2 tracking-tight'
@@ -133,15 +133,15 @@ const AboutMain = () => {
                 variants={slideIn('left', 'tween', 0.2, 0.5)}
                 className='md:mt-4 ss:mt-2 mt-0'>
                 <   img src={img3} alt='bed_cover'
-                    className=' md:h-[350px] ss:h-[300px] h-[280px] w-auto'
+                    className=' md:h-[350px] ss:h-[250px] h-[280px] w-auto'
                 />
             </motion.div>
             
             <motion.div variants={textVariant()}
-            className='flex-1 ml-40'
+            className='flex-1 md:ml-40 ss:ml-20'
             >
                 <h1 className='text-primary font-bold md:text-[45px]
-                ss:text-[40px] text-[40px] tracking-tight'>
+                ss:text-[35px] text-[40px] tracking-tight'>
                     PVC Transparent
                 </h1>
                 
@@ -150,20 +150,20 @@ const AboutMain = () => {
                     w-[100px] md:h-[13px] ss:h-[10px] h-[10px]' />
                 </div>
 
-                <p className='md:mt-8 ss:mt-8 mt-5 text-primary md:text-[20px] 
-                ss:text-[20px] text-[14px] md:max-w-[650px] ss:max-w-[700px]
+                <p className='md:mt-8 ss:mt-5 mt-5 text-primary md:text-[20px] 
+                ss:text-[16px] text-[14px] md:max-w-[650px] ss:max-w-[700px]
                 tracking-tight'>
                 PVC Transparent description to be inserted here.
                 </p>
 
-                <div className='md:mt-12 ss:mt-8 mt-5'>
+                <div className='md:mt-12 ss:mt-5 mt-5'>
                     <div className='border-t-[1px] border-t-primary' />
                 </div>
 
-                <div className='flex md:mt-12 ss:mt-8 mt-5 md:gap-5 
+                <div className='flex md:mt-12 ss:mt-5 mt-5 md:gap-5 
                 ss:gap-2 gap-2'>
                     <button className='bg-primary md:text-[17px] 
-                    ss:text-[17px] text-[12px] md:py-2 ss:py-2 py-1 
+                    ss:text-[14px] text-[12px] md:py-2 ss:py-2 py-1 
                     md:px-12 ss:px-8 px-3 text-white tracking-tight
                     rounded-[3px] font-medium border-none grow2'
                     onClick={() => navigate('/contact')}>
@@ -171,7 +171,7 @@ const AboutMain = () => {
                     </button>
 
                     <button className='border-[1px] border-primary 
-                    md:text-[17px] ss:text-[17px] text-[12px] 
+                    md:text-[17px] ss:text-[14px] text-[12px] 
                     md:py-2 ss:py-2 py-1 md:px-12 ss:px-8 
                     px-3 text-primary rounded-[3px] font-medium 
                     grow2 tracking-tight'
@@ -189,15 +189,15 @@ const AboutMain = () => {
                 variants={slideIn('left', 'tween', 0.2, 0.5)}
                 className='md:mt-4 ss:mt-2 mt-0'>
                 <   img src={img4} alt='bed_cover'
-                    className=' md:h-[350px] ss:h-[300px] h-[280px] w-auto'
+                    className=' md:h-[350px] ss:h-[250px] h-[280px] w-auto'
                 />
             </motion.div>
             
             <motion.div variants={textVariant()}
-            className='flex-1 ml-40'
+            className='flex-1 md:ml-40 ss:ml-20'
             >
                 <h1 className='text-primary font-bold md:text-[45px]
-                ss:text-[40px] text-[40px] tracking-tight'>
+                ss:text-[35px] text-[40px] tracking-tight'>
                     PVC Leather
                 </h1>
                 
@@ -206,20 +206,20 @@ const AboutMain = () => {
                     w-[100px] md:h-[13px] ss:h-[10px] h-[10px]' />
                 </div>
 
-                <p className='md:mt-8 ss:mt-8 mt-5 text-primary md:text-[20px] 
-                ss:text-[20px] text-[14px] md:max-w-[650px] ss:max-w-[700px]
+                <p className='md:mt-8 ss:mt-5 mt-5 text-primary md:text-[20px] 
+                ss:text-[16px] text-[14px] md:max-w-[650px] ss:max-w-[700px]
                 tracking-tight'>
                 PVC Leather description to be inserted here.
                 </p>
 
-                <div className='md:mt-12 ss:mt-8 mt-5'>
+                <div className='md:mt-12 ss:mt-5 mt-5'>
                     <div className='border-t-[1px] border-t-primary' />
                 </div>
 
-                <div className='flex md:mt-12 ss:mt-8 mt-5 md:gap-5 
+                <div className='flex md:mt-12 ss:mt-5 mt-5 md:gap-5 
                 ss:gap-2 gap-2'>
                     <button className='bg-primary md:text-[17px] 
-                    ss:text-[17px] text-[12px] md:py-2 ss:py-2 py-1 
+                    ss:text-[14px] text-[12px] md:py-2 ss:py-2 py-1 
                     md:px-12 ss:px-8 px-3 text-white tracking-tight
                     rounded-[3px] font-medium border-none grow2'
                     onClick={() => navigate('/contact')}>
@@ -227,7 +227,7 @@ const AboutMain = () => {
                     </button>
 
                     <button className='border-[1px] border-primary 
-                    md:text-[17px] ss:text-[17px] text-[12px] 
+                    md:text-[17px] ss:text-[14px] text-[12px] 
                     md:py-2 ss:py-2 py-1 md:px-12 ss:px-8 
                     px-3 text-primary rounded-[3px] font-medium 
                     grow2 tracking-tight'
@@ -245,15 +245,15 @@ const AboutMain = () => {
                 variants={slideIn('left', 'tween', 0.2, 0.5)}
                 className='md:mt-4 ss:mt-2 mt-0'>
                 <   img src={cellotapes} alt='bed_cover'
-                    className=' md:h-[350px] ss:h-[300px] h-[280px] w-auto'
+                    className=' md:h-[350px] ss:h-[250px] h-[280px] w-auto'
                 />
             </motion.div>
             
             <motion.div variants={textVariant()}
-            className='flex-1 ml-40'
+            className='flex-1 md:ml-40 ss:ml-20'
             >
                 <h1 className='text-primary font-bold md:text-[45px]
-                ss:text-[40px] text-[40px] tracking-tight'>
+                ss:text-[35px] text-[40px] tracking-tight'>
                     Cellotape
                 </h1>
                 
@@ -262,20 +262,20 @@ const AboutMain = () => {
                     w-[100px] md:h-[13px] ss:h-[10px] h-[10px]' />
                 </div>
 
-                <p className='md:mt-8 ss:mt-8 mt-5 text-primary md:text-[20px] 
-                ss:text-[20px] text-[14px] md:max-w-[650px] ss:max-w-[700px]
+                <p className='md:mt-8 ss:mt-5 mt-5 text-primary md:text-[20px] 
+                ss:text-[16px] text-[14px] md:max-w-[650px] ss:max-w-[700px]
                 tracking-tight'>
                 Cellotape description to be inserted here.
                 </p>
 
-                <div className='md:mt-12 ss:mt-8 mt-5'>
+                <div className='md:mt-12 ss:mt-5 mt-5'>
                     <div className='border-t-[1px] border-t-primary' />
                 </div>
 
-                <div className='flex md:mt-12 ss:mt-8 mt-5 md:gap-5 
+                <div className='flex md:mt-12 ss:mt-5 mt-5 md:gap-5 
                 ss:gap-2 gap-2'>
                     <button className='bg-primary md:text-[17px] 
-                    ss:text-[17px] text-[12px] md:py-2 ss:py-2 py-1 
+                    ss:text-[14px] text-[12px] md:py-2 ss:py-2 py-1 
                     md:px-12 ss:px-8 px-3 text-white tracking-tight
                     rounded-[3px] font-medium border-none grow2'
                     onClick={() => navigate('/contact')}>
@@ -283,7 +283,7 @@ const AboutMain = () => {
                     </button>
 
                     <button className='border-[1px] border-primary 
-                    md:text-[17px] ss:text-[17px] text-[12px] 
+                    md:text-[17px] ss:text-[14px] text-[12px] 
                     md:py-2 ss:py-2 py-1 md:px-12 ss:px-8 
                     px-3 text-primary rounded-[3px] font-medium 
                     grow2 tracking-tight'

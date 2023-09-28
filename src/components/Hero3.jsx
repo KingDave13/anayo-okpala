1fccf79f825c06310b73fbf24c1474f86492322f
David Okaliwe <okaliwedavid@gmail.com>
2023-09-28 09:29:57 +0100

    Update

--- src/components/Hero3.jsx
+++ src/components/Hero3.jsx
@@ -21,14 +21,43 @@ const Hero = () => {
                 </h1>
 
                 <div className='flex flex-row items-center md:gap-5 
-                ss:gap-5 gap-2 text-white md:text-[20px] ss:text-[20px] 
+                ss:gap-5 gap-1 text-white md:text-[20px] ss:text-[20px] 
                 text-[14px] justify-center'>
                     <p>Home</p>
                     <BsArrowRightShort className='md:text-[30px] 
                     ss:text-[30px] text-[25px] text-white'/>
                     <p>Contact</p>
                 </div>
-              </motion.div>          
+
+                <div className='md:mt-10 ss:mt-7 mt-5 text-center'>
+                  <p className='text-white font-normal md:text-[23px]
+                  ss:text-[22px] text-[15px]'>
+                    <span className='text-secondary font-bold'>Email:
+                    </span> Anasconigltd@yahoo.com
+                  </p>
+
+                  <p className='text-white font-normal md:text-[23px]
+                  ss:text-[20px] text-[14px] md:mt-2 ss:mt-1 mt-1'>
+                    <span className='text-secondary font-bold'>Phone:
+                    </span> 08037002296, 08035895253, 08134353345.
+                  </p>
+
+                  <p className='text-white font-normal md:text-[23px]
+                  ss:text-[20px] text-[14px] leading-[17px] md:mt-3
+                  ss:mt-2 mt-1'>
+                    <span className='text-secondary font-bold'>Head Office:
+                    </span> No. 101 Ibrahim Taiwo Road by Sani Ungogo,
+                    Kano State.
+                  </p>
+
+                  <p className='text-white font-normal md:text-[23px]
+                  ss:text-[20px] text-[14px] leading-[17px] md:mt-5
+                  ss:mt-4 mt-1'>
+                    <span className='text-secondary font-bold'>Branch Office:
+                    </span> No 9 Festing Road, Sabon Gari, Kano State.
+                  </p>
+                </div>
+              </motion.div>
           </div>
       </section>  
     )

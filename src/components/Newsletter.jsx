28c7e84c8eb8f03dd6271e1a96c018c00f952eec
David Okaliwe <okaliwedavid@gmail.com>
2023-10-02 08:08:48 +0100

    Update

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -10,7 +10,7 @@ const Newsletter = () => {
       relative items-center justify-center flex-col'>
         <motion.div variants={textVariant()} className='flex flex-col 
         relative w-full items-center'>
-            <h1 className='text-primary font-bold md:text-[50px]
+            <h1 className='text-primary font-bold md:text-[45px]
                 ss:text-[40px] text-[30px] tracking-tight'>
                 Subscribe to our newsletter
             </h1>
@@ -27,26 +27,28 @@ const Newsletter = () => {
         </motion.div>
 
         <motion.div variants={fadeIn('up', 'spring', 0.3)} 
-        className='flex flex-col w-full items-center'>
+        className='flex flex-col w-full items-center md:mt-10 ss:mt-5 
+        mt-5'>
             <input
               type="email"
               name="email"
             //   value={form.email}
             //   onChange={handleChange}
               placeholder="Enter your email"
-              className="py-4 px-6 border-[1px] outline-none 
-              placeholder:text-black text-black rounded-lg
+              className="py-4 px-4 border-b-[2px] outline-none 
+              placeholder:text-black-100 text-black rounded-lg
               md:placeholder:text-[16px] ss:placeholder:text-[14px]
-              placeholder:text-[14px]"
+              placeholder:text-[14px] md:w-[650px] text-center"
             />
 
-            <button className='bg-primary grow
-                md:text-[17px] ss:text-[17px] text-[13px] md:py-3 
-                ss:py-3 py-2 md:px-14 ss:px-14 px-8 text-white 
-                rounded-[3px] font-medium border-none hover:text-primary'
-                >
-                    Subscribe
-                </button>
+            <button className='bg-primary grow2 md:mt-5 ss:mt-5 mt-5
+            md:text-[16px] ss:text-[16px] text-[12px] md:py-3 
+            ss:py-3 py-2 md:px-14 ss:px-14 px-8 text-white text-center
+            rounded-[3px] font-medium border-none hover:text-primary
+            hover:text-white'
+            >
+                Subscribe
+            </button>
         </motion.div>
       </div>
     </section>

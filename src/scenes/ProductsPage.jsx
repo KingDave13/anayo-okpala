6d92879d0a9f4ae6030f2cfe8bed00c2dfff9b0f
David Okaliwe <okaliwedavid@gmail.com>
2023-10-06 09:54:46 +0100

    final updates

--- src/scenes/ProductsPage.jsx
+++ src/scenes/ProductsPage.jsx
@@ -4,37 +4,37 @@ import {
     Footer, 
     Hero4, 
     ProductsMain, 
-    Newsletter} 
+    Newsletter } 
     from '../components';
   import { img2 } from '../assets';
   
-  const AboutPage = () => {
-    return (
-      <div className='font-instrument-sans'>
-        <NavbarAlt3 />
-        <div className='relative flex items-center justify-center w-full 
-        md:max-h-[450px] ss:max-h-[300px] max-h-[200px] top-20'>
-          <img src={img2} alt='product3'
-          className='object-cover w-[150rem] md:h-[450px] ss:h-[300px]
-          h-[200px]' />
-          <div className='absolute w-full'>
-            <Hero4 />
-          </div>
+const AboutPage = () => {
+  return (
+    <div className='font-instrument-sans'>
+      <NavbarAlt3 />
+      <div className='relative flex items-center justify-center w-full 
+      md:max-h-[450px] ss:max-h-[300px] max-h-[200px] top-20'>
+        <img src={img2} alt='product3'
+        className='object-cover w-[150rem] md:h-[450px] ss:h-[300px]
+        h-[200px]' />
+        <div className='absolute w-full'>
+          <Hero4 />
         </div>
-        
-        <div className='bg-dimWhite'>
-            <ProductsMain />
-        </div>
-        
-        <CTA />
-
-        <div className='bg-dimWhite'>
-          <Newsletter />
-        </div>
-
-        <Footer />
       </div>
-    )
-  };
-  
-  export default AboutPage;
+      
+      <div className='bg-dimWhite'>
+          <ProductsMain />
+      </div>
+      
+      <CTA />
+
+      <div className='bg-dimWhite'>
+        <Newsletter />
+      </div>
+
+      <Footer />
+    </div>
+  )
+};
+
+export default AboutPage;

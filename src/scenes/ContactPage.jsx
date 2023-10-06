18e7ce249b91c6eae313ed31c555ec603efe0bb6
David Okaliwe <okaliwedavid@gmail.com>
2023-10-06 09:53:13 +0100

    final updates

--- src/scenes/ContactPage.jsx
+++ src/scenes/ContactPage.jsx
@@ -3,33 +3,33 @@ import {
     Footer, 
     Hero3, 
     Contact, 
-    Newsletter} 
+    Newsletter } 
     from '../components';
 import { product3 } from '../assets';
 
-  const ContactPage = () => {
-    return (
-      <div className='font-instrument-sans'>
-        <NavbarAlt2 />
-        <div className='relative flex items-center justify-center w-full 
-        md:max-h-[450px] ss:max-h-[300px] max-h-[200px] top-20'>
-          <img src={product3} alt='product3'
-          className='object-cover w-[150rem] md:h-[450px] ss:h-[300px]
-          h-[200px]' />
-          <div className='absolute w-full'>
-            <Hero3 />
-          </div>
+const ContactPage = () => {
+  return (
+    <div className='font-instrument-sans'>
+      <NavbarAlt2 />
+      <div className='relative flex items-center justify-center w-full 
+      md:max-h-[450px] ss:max-h-[300px] max-h-[200px] top-20'>
+        <img src={product3} alt='product3'
+        className='object-cover w-[150rem] md:h-[450px] ss:h-[300px]
+        h-[200px]' />
+        <div className='absolute w-full'>
+          <Hero3 />
         </div>
-        
-        <Contact />
-
-        <div className='bg-dimWhite'>
-          <Newsletter />
-        </div>
-
-        <Footer />
       </div>
-    )
-  };
-  
-  export default ContactPage;
+      
+      <Contact />
+
+      <div className='bg-dimWhite'>
+        <Newsletter />
+      </div>
+
+      <Footer />
+    </div>
+  )
+};
+
+export default ContactPage;
